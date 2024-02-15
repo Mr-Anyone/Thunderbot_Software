36a16f859e5c62a36a3ab66e94c766ebc71513ff
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2024-02-14 22:57:40 -0800

    kind of get protobuf to work

---
+++ src/proto/test_build.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+// #include "play.pb.h"
+// #include "pb_encode.h"
+// #include "geometry.pb.h"
+
+int main(){
+    std::cout << "Hello World" << std::endl; 
+
+    std::cout << "Program Ended" << std::endl; 
+    return 0;
+}
