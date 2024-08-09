57f852d7ccfcae4704c9b6f0c1c27a26a6eae154
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2024-08-09 07:30:02 -0700

    fstream worked

--- src/test_build.cpp
+++ src/test_build.cpp
@@ -1,7 +1,27 @@
 #include <iostream> 
+#include <fstream> 
+
 
 int main(){
     std::cout << "Hello World" << std::endl; 
 
+    // Create an ofstream object to handle file output
+    std::ofstream outFile("/tmp/example.txt");
+
+    // Check if the file was created/opened successfully
+    if (!outFile) {
+        std::cerr << "Error opening file for writing." << std::endl;
+        return 1;
+    }
+
+    // Write data to the file
+    outFile << "Hello, world!" << std::endl;
+    outFile << "This is a line of text." << std::endl;
+    outFile << "This is another line of text." << std::endl;
+
+    // Close the file
+    outFile.close();
+
+    std::cout << "I've finished running!" << std::endl; 
     return 0;
 }
