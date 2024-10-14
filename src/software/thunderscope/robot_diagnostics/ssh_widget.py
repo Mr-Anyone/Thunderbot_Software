e0e861c8d6875d6f30202d90320f89beb3dba2ab
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2024-10-14 09:34:10 +0200

    added some changes

--- src/software/thunderscope/robot_diagnostics/ssh_widget.py
+++ src/software/thunderscope/robot_diagnostics/ssh_widget.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import socket
 import subprocess
 import pyqtgraph as pg
@@ -6,20 +8,20 @@ from pyqtgraph.Qt.QtWidgets import *
 from software.py_constants import *
 import time as time
 
+
 class SSHWidget(QWidget):
     def __init__(self, robot_id: int) -> None:
         super().__init__()
 
-        self.robot_id = robot_id 
+        self.robot_id = robot_id
         # TODO: fix this later!
-        name = "bruhbot"
-        
+        # 7 is balle
+        name = "balle"
+
         self.hostnames = [f"{name}{sufix}.local" for sufix in ["jetson", ""]]
-        self.password = "nice try" 
+        self.password = "nice try"
 
-
-        self.pinable_widget = QLabel("Ping: No Good")
-        self.sshable_widget = QLabel("SSH Status: No Good")
+        self.pingable_widget = QLabel("Ping: No Good")
         self.systemd_widget = QLabel("Systemd Status: Active")
         self.robot_channel = QLabel("Robot Channel: Active")
         self.flash_button = QPushButton("Flash Robot")
@@ -28,14 +30,14 @@ class SSHWidget(QWidget):
         self.systemd_output = QLabel("systemd outout:")
 
         self.enter_password_widget.textChanged.connect(self.set_password)
-        self.flash_button.clicked.connect(self.check_systemd_status)
+        self.flash_button.clicked.connect(lambda x: x)
+
 
         self.fullLayout = QVBoxLayout()
         self.layout = QHBoxLayout()
 
         # adding the widget into stuff
-        self.layout.addWidget(self.pinable_widget)
-        self.layout.addWidget(self.sshable_widget)
+        self.layout.addWidget(self.pingable_widget)
         self.layout.addWidget(self.systemd_widget)
         self.layout.addWidget(self.robot_channel)
         self.layout.addWidget(self.flash_button)
@@ -45,51 +47,118 @@ class SSHWidget(QWidget):
         self.fullLayout.addLayout(self.layout)
         self.setLayout(self.fullLayout)
 
+        self.daemon_thread = threading.Thread(target=self.check)
+        self.daemon_thread.start()
+
     def set_password(self):
-        # we can now set password 
+        # we can now set password
         self.password = self.enter_password_widget.toPlainText()
+
+    def check(self):
+        """
+        this is going to check for stuff I guess
+        """
+
+        while True:
+            # I can ping
+            if self.is_pingable():
+                print("I can ping")
+                self.pingable_widget.setText("Ping: Good")
+            else:
+                print("I cannot ping!")
+                self.pingable_widget.setText("Ping: No Good")
+                # there is no way you can ping but not ssh
+                return
+
+            # what is the systmemd widget?
+            if self.check_systemd_status():
+                self.systemd_widget.setText("Systmed Status: Active")
+            else:
+                self.systemd_widget.setText("Systmed Status: Inactive")
+
+            # TODO: the following what is the robot channel id?
+            channel_id = self.get_channel_id()
+            self.robot_channel.setText(f"Chanel id: {str(channel_id)}")
+
+            time.sleep(10)
 
     def send_command_ssh(self, command):
         for hostname in self.hostnames:
             try:
                 # Construct the SSH command
-                ssh_command = ['sshpass', '-p', self.password, 'ssh', f'robot@{hostname}', command]
+                ssh_command = [
+                    "sshpass",
+                    "-p",
+                    self.password,
+                    "ssh",
+                    f"robot@{hostname}",
+                    command,
+                ]
                 print(f"sending ssh command{ssh_command}")
 
                 # Run the command
-                result = subprocess.run(ssh_command, capture_output=True, text=True, check=True)
+                result = subprocess.run(
+                    ssh_command, capture_output=True, text=True, check=False, timeout=5
+                )
                 print(f"the result is : {result.stdout}")
 
                 # Print the output and errors
                 if result.stdout:
                     return result.stdout
 
-            except Exception as e: 
+            except Exception as e:
                 print("I got exception e: {}".format(e))
 
         raise Exception("cannot send command through ssh")
+
+    # TODO:
+    def stop_thunderloop(self):
+        pass
+
+    # TODO:
+    def restart_thunderloop(self):
+        pass
 
     def is_systemd_active_from_text(self, text):
         return "active (running)" in text
 
     def check_systemd_status(self):
         try:
-            systemd_output = self.send_command_ssh(f"echo {self.password} | sudo -S systemctl status thunderloop")
+            systemd_output = self.send_command_ssh(
+                f"echo {self.password} | sudo -S systemctl status thunderloop"
+            )
 
             self.systemd_output.setText(systemd_output)
+            # set when the systemd output is Active
             if self.is_systemd_active_from_text(systemd_output):
-                self.systemd_widget.setText("systemd: active") 
+                return True
 
+            return False
+            # what to do when there is an error?
         except Exception as e:
             print("I got an exception e: {}".format(e))
-
-    def is_pinable(self):
-        # this is the ssh port and this should work?
-        port = 22
-        timeout = 5
-        try:
-            socket.create_connection((host, port), timeout)
-            return True
-        except (socket.timeout, OSError):
             return False
 
+    def is_pingable(self):
+        # this should work if you create an ssh connection
+        port = 22
+        timeout = 1
+
+        for host in self.hostnames:
+            try:
+                socket.create_connection((host, port), timeout)
+                return True
+            except (socket.timeout, OSError):
+                print("timeout for: {}".format(host))
+                continue
+
+        return False
+
+    def get_channel_id(self):
+        try:
+            channel_id = self.send_command_ssh("redis-cli get /channel_id").replace(
+                '"', ""
+            )
+            return int(channel_id)
+        except Exception as e:
+            return -1
